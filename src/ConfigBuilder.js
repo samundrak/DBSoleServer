2f76264e9c9755bd9d017a6590b4d7b8663129ed
alicelimbu <alice.limbu@introcept.co>
2016-09-16 16:25:11 +0545

    wip

--- src/ConfigBuilder.js
+++ src/ConfigBuilder.js
@@ -1,13 +1,23 @@
 const inquirer = require('inquirer');
 const InquirerConfigBuilder = require('inquirer_config_builder');
 const fs = require('fs');
+const os =  require('os');
+const path = require('path');
 
 module.exports = class ConfigBuilder {
 
     constructor() {
-        this.configPath = 'config.json';
+        this.configPath = path.join(os.homedir(),'.dbsole/config.json');
     }
 
+    getConfigPath(){
+        return this.configPath;
+    }
+
+    getConfig(){
+        return require(this.configPath);
+    }
+    
     promptForConfig(schema, cb) {
         let questions = InquirerConfigBuilder.questions(schema);
 
@@ -30,6 +40,7 @@ module.exports = class ConfigBuilder {
     }
 
     createConfig(config) {
+        fs.mkdirSync(path.join(os.homedir(),'.dbsole'));
         return fs.writeFileSync(this.configPath, JSON.stringify(config));
     }
 }
